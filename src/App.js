1a8a897333d84afb4614648a4ab4c21ccba1b994
Shaileshkumar Dahekar <107181032+skd108k@users.noreply.github.com>
2023-01-24 01:05:56 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+ 
+import UseState from './component/Hooks/UseState'
+import UseEffect from './component/Hooks/UseEffect'
+import UseReducer from './component/Hooks/UseReducer'
+ 
+
+const App = () => {
+  
+    return <UseState />
+    return <UseEffect />
+  return <UseReducer />;
+ 
+}
+export default App
